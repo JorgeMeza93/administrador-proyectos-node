caf136439a7060dab949988aa0a2a2815ccb4f96
Jorge Meza Bello <jorberiver@gmail.com>
2023-03-30 23:21:19 -0600

    creado controlador y router de usuarios

---
+++ backend/controller/controladorUsuarios.js
@@ -0,0 +1,7 @@
+
+
+const obtenerUsuarios = (req, res) => {
+    res.json({ msg: "Hola Amigos" });
+}
+
+export { obtenerUsuarios }
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import conectarBD from "./config/db.js";
 import dotenv from "dotenv";
+import usuarioRouter from "./routes/usuarioRoutes.js";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 conectarBD();
 
+app.use("/api/usuarios", usuarioRouter)
 app.listen(PORT, () => {
     console.log(`Servidor ha iniciado en el puerto ${PORT}`);
 })
