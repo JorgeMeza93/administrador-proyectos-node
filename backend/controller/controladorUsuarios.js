60a18d9e430d98bf01955d34343c256b621366bd
Jorge Meza Bello <jorberiver@gmail.com>
2023-05-11 01:40:25 -0600

    Comprobar tpken y confirmar usuario

--- backend/controller/controladorUsuarios.js
+++ backend/controller/controladorUsuarios.js
@@ -48,7 +48,22 @@ const autenticar = async (req, res) => {
     }
 }
 const confirmar = async (req, res) => {
-    
+    const { token } = req.params;
+    const usuarioConfirmar = await Usuario.findOne({token});
+    if(!usuarioConfirmar){
+        const error = new Error("Token no válido");
+        return res.status(403).json({ msg: error.message });
+    }
+    console.log("Encontrado");
+    try {
+        usuarioConfirmar.confirmado = true;
+        usuarioConfirmar.token = "";
+        await usuarioConfirmar.save();
+        res.json({ msg: "Usuario confirmado correctamente" });
+        
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export { obtenerUsuarios, registrar, autenticar, confirmar }
